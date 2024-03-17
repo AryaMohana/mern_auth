06a55699ec4699826e5b973458915393d02a9c90
Mohanachandran Arya <aryahanachandran@gmail.com>
2024-03-17 18:41:20 +0530

    installed react

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-red-600  text-3xl font-bold underline ">heloo world</h1>
+    </div>
+  )
+}
